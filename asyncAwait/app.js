432760da4a458cf9e9a0a430202847f273e2040c
Abhay Mishra <abhaymishra72501@gmail.com>
2026-07-27 12:23:11 +0530

    Add app.js

--- asyncAwait/app.js
+++ asyncAwait/app.js
@@ -1,22 +1,67 @@
-// Async Await 1 
- function paymentStatus(){
-    return new Promise((resolve,reject)=>{
-        let val = Math.random();
-        if(val > 0.5){
-            resolve("Payment Sucessful!");
+// // Async Await 1 
+//  function paymentStatus(){
+//     return new Promise((resolve,reject)=>{
+//         let val = Math.random();
+//         if(val > 0.5){
+//             resolve("Payment Sucessful!");
 
-        }
-            reject("Payment failed!");
+//         }
+//             reject("Payment failed!");
         
+//     });
+// }
+// (async function checkPayment() {
+//     try{
+//         let msg = await paymentStatus();
+//         console.log(msg);
+        
+//     } catch(err){
+//         console.log(err);
+        
+//     }
+// })();
+
+// Async Await 2
+
+function takeOrder(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+          console.log("Pizza Ordered");
+            resolve();
+        }, 3000);
     });
 }
-(async function checkPayment() {
-    try{
-        let msg = await paymentStatus();
-        console.log(msg);
-        
-    } catch(err){
-        console.log(err);
-        
-    }
+function preparePizza(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+          console.log("Pizza Prepared");
+            resolve();
+        }, 3000);
+    });
+}
+function bakePizza(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+          console.log("Pizza baked");
+            resolve();
+        }, 3000);
+    });
+}
+
+function deliverPizza(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(() => {
+          console.log("Pizza Delivered");
+            resolve();
+        }, 3000);
+    });
+}
+
+(async function pizzaOrder() {
+
+    await takeOrder();
+    await preparePizza();
+    await bakePizza();
+    await deliverPizza();
+    
 })();
---
+++ asyncAwait/tempCodeRunnerFile.js
@@ -0,0 +1,22 @@
+// Async Await 1 
+//  function paymentStatus(){
+//     return new Promise((resolve,reject)=>{
+//         let val = Math.random();
+//         if(val > 0.5){
+//             resolve("Payment Sucessful!");
+
+//         }
+//             reject("Payment failed!");
+        
+//     });
+// }
+// (async function checkPayment() {
+//     try{
+//         let msg = await paymentStatus();
+//         console.log(msg);
+        
+//     } catch(err){
+//         console.log(err);
+        
+//     }
+// })();
